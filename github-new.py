282e5382e0cac52efc50f631b50210e578b0b949
Shaleen Baral <shaleenbaral111@gmail.com>
2019-06-07 22:41:21 +0545

    Everything works now

--- github-new.py
+++ github-new.py
@@ -1,17 +1,20 @@
 from getpass import getpass
+from helper import sinput
+from json import dumps
 from requests import get, post
 
-username = input("Username : ")
+username = input("Username: ")
 password = getpass()
 
 response = get("https://api.github.com/user", auth=(username, password))
 
-print(f"Authentication Status : {response}")
+print(f"Authentication Status : {response.status_code}")
 
-name = input("Enter Repository Name : ")
-private = input("Private (True/False) : ")
+name = input("Enter Repository Name: ")
+private = bool(sinput("Private (True/False): ", ("true", "false")))
+payload = {"name": name, "private": private, "autoinit": true}
 
-reponse = post("https://api.github.com/user/repos",
-               data={"name": name, "private": private})
+response = post("https://api.github.com/user/repos",
+                data=dumps(payload), auth=(username, password))
 
-print(f"Repository Creation Status : {response}")
+print(f"Repository Creation Status : {response.status_code}")
--- helper.py
+++ helper.py
@@ -1,8 +1,24 @@
+def all_low_tup(tup):
+    low_tup = ()
+    for el in tup:
+        # You can't update a tuple
+        # But you can perform addition
+        # and reassignment
+        #   low_tup = low_tup + tuple(el.lower())
+        # You can also unpack a tupple using * operator
+        low_tup = (*low_tup, el.lower())
+    return low_tup
+
+
 def sinput(prompt, allowed):
     # Sinput = Strong Input
     # Allows the User to Only Input the Given Words
     # List of Allowed Words is A Tuple
-    resp = input(prompt)
+    allowed = all_low_tup(allowed)
+    resp = input(prompt).strip().lower()
     while resp not in allowed:
         resp = input(prompt)
     return resp
+
+if __name__ == "__main__":
+    pass
