639896f976541ea00f826039bad8e3acc800dc45
Shaleen Baral <shaleenbaral111@gmail.com>
2019-06-07 19:33:19 +0545

    Basic Program Done

---
+++ github-new.py
@@ -0,0 +1,18 @@
+import requests
+from getpass import getpass
+
+username = input("Enter your Username for Github : ")
+password = getpass()
+
+response = requests.get("https://api.github.com/user",
+                        auth=(username, password))
+
+print(f"Authentication Status : {response}")
+
+name = input("Enter Repository Name : ")
+private = input("Private (Yes/No) : ")
+
+reponse = requests.post("https://api.github.com/user/repos",
+                        data={"name": name, "private": private})
+
+print(f"Repository Creation Status : {response}")
