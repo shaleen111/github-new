ef10143af529c26e818ef7d60b7480a6e3b2d3e6
Shaleen Baral <shaleenbaral111@gmail.com>
2019-06-08 15:45:35 +0545

    Removed username/pw integration

--- github-new.py
+++ github-new.py
@@ -1,4 +1,5 @@
 import argparse
+from helpers import sinput
 from json import dump, dumps, load
 from requests import get, post
 from sys import exit
@@ -8,7 +9,7 @@ def response_handler(action, response, accepted):
     status = response.status_code
     if status not in accepted:
         msg = response.json()["message"]
-        print(f"Error {action} failed with code {status} ({msg})")
+        print(f"Error: {action} failed with code {status} ({msg})")
         exit()
     else:
         print(f"{action} Status: {status} (Successful)")
@@ -20,11 +21,11 @@ def create_rep(name, private, token):
     if private is None:
         private = bool(sinput("Private (True/False): ", ("true", "false")))
 
-    payload = {"name": name, "private": private, "autoinit": true}
+    payload = {"name": name, "private": private, "autoinit": True}
 
     response = post("https://api.github.com/user/repos",
                     data=dumps(payload),
-                    header={"Authorization": f"token {token}"})
+                    headers={"Authorization": f"token {token}"})
 
     response_handler("Repository Creation", response, (200, 201))
 
@@ -42,16 +43,18 @@ def main():
                         help="Alter configs only")
     parser.add_argument("-n", "--name", type=str, default=None,
                         help="Give repository Name")
-    parser.add_argument("-p", "--private", action="store_true", default=False,
+    parser.add_argument("-p", "--private", action="store_true", default=None,
                         help="Sets repository to be Private")
 
     args = parser.parse_args()
-
     print(args.private)
-    if args.config is False:
-        pass
-
-    if args.oauth is None:
+    if args.config is True:
+        if (not args.name and args.private is not None) or \
+           (args.oauth and not args.save):
+            print("Error: Cannot Create Repositories in Config Mode")
+            print("       Run program without the c flag!")
+            exit()
+    elif args.oauth is None:
         with open("config.txt", "r") as c:
             config = load(c)
             args.oauth = config["token"]
